97951b77c25b6a6551140796d8f65ce0f7d462aa
William <WiWang@nltechdev.com>
2018-05-10 15:30:35 +0800

    WorkLine

--- SpeechEnTxt/SpeechEnTxt/MainForm.cs
+++ SpeechEnTxt/SpeechEnTxt/MainForm.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Speech.Synthesis;
+
 namespace SpeechEnTxt
 {
     public partial class frmMainForm : Form
