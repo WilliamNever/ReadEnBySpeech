e3d6ee8daeaf6521e61d68d68c542d6f1271d2f5
William <WiWang@nltechdev.com>
2018-05-11 15:15:43 +0800

    add ControlBoard

---
+++ SpeechEnTxt/SpeechEnTxt/Classes/DomainClasses/ControlBoard.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SpeechEnTxt.Classes.DomainClasses
+{
+    partial class ControlBoard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbVoices = new System.Windows.Forms.ComboBox();
+            this.cbkRead = new System.Windows.Forms.CheckBox();
+            this.cbkRecord = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Voices:";
+            // 
+            // cmbVoices
+            // 
+            this.cmbVoices.FormattingEnabled = true;
+            this.cmbVoices.Location = new System.Drawing.Point(58, 6);
+            this.cmbVoices.Name = "cmbVoices";
+            this.cmbVoices.Size = new System.Drawing.Size(240, 21);
+            this.cmbVoices.TabIndex = 1;
+            // 
+            // cbkRead
+            // 
+            this.cbkRead.AutoSize = true;
+            this.cbkRead.Location = new System.Drawing.Point(315, 8);
+            this.cbkRead.Name = "cbkRead";
+            this.cbkRead.Size = new System.Drawing.Size(52, 17);
+            this.cbkRead.TabIndex = 2;
+            this.cbkRead.Text = "Read";
+            this.cbkRead.UseVisualStyleBackColor = true;
+            // 
+            // cbkRecord
+            // 
+            this.cbkRecord.AutoSize = true;
+            this.cbkRecord.Location = new System.Drawing.Point(15, 134);
+            this.cbkRecord.Name = "cbkRecord";
+            this.cbkRecord.Size = new System.Drawing.Size(61, 17);
+            this.cbkRecord.TabIndex = 3;
+            this.cbkRecord.Text = "Record";
+            this.cbkRecord.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Speed:";
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.LargeChange = 2;
+            this.tbSpeed.Location = new System.Drawing.Point(58, 39);
+            this.tbSpeed.Minimum = -10;
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(309, 45);
+            this.tbSpeed.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Volume:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(58, 83);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(309, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Value = 60;
+            // 
+            // ControlBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbkRecord);
+            this.Controls.Add(this.cbkRead);
+            this.Controls.Add(this.cmbVoices);
+            this.Controls.Add(this.label1);
+            this.Name = "ControlBoard";
+            this.Size = new System.Drawing.Size(379, 197);
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbVoices;
+        private System.Windows.Forms.CheckBox cbkRead;
+        private System.Windows.Forms.CheckBox cbkRecord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
---
+++ SpeechEnTxt/SpeechEnTxt/Classes/DomainClasses/ControlBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeechEnTxt.Classes.DomainClasses
+{
+    public partial class ControlBoard : UserControl
+    {
+        public ControlBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}
